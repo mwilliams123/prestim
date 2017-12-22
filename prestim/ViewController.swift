bb9936dda8e6f561ab94b168115ea3b69ca67a86
Megan Williams <meganwilliams@Megans-MacBook-Pro.local>
2017-12-21 23:16:46 -0700

    Initial Commit

---
+++ prestim/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  prestim
+//
+//  Created by Megan Williams on 12/21/17.
+//  Copyright © 2017 Prestimulus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
